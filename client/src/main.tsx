293f794cccc1da7c5cc236559c6c6d5ed3bb02ce
Senior Son <son.tkhong@outlook.com>
2025-02-13 10:47:10 +0100

    add navbar, tailwind merger, react router dom

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const App = () => {
+  return (
+    <div className="w-full p-6">
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default App;
